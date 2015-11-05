8a4c98588344340a0e251a325e76e646458e343c
Ladislav Thon <lthon@redhat.com>
2015-11-05 13:28:09 +0100

    improve documentation and code style of the Add/RemoveModule commands

--- commands/src/main/java/org/wildfly/extras/creaper/commands/modules/RemoveModule.java
+++ commands/src/main/java/org/wildfly/extras/creaper/commands/modules/RemoveModule.java
@@ -5,30 +5,35 @@ import org.wildfly.extras.creaper.core.online.OnlineCommand;
 import org.wildfly.extras.creaper.core.online.OnlineCommandContext;
 
 /**
- * <p>The command is used to remove modules</p>
- *
- * <p>When a module is removed, its module.xml and other resources will be removed from the module repository
- * as well as its directory structure up to the point where other modules met.</p>
+ * <p>Removes a module (in the JBoss Modules sense) from the module repository. The {@code module.xml} file
+ * and other resources will be removed from the module repository as well as the directory structure up to the point
+ * where other modules meet.</p>
  */
 public final class RemoveModule implements OnlineCommand {
     private final String moduleName;
     private final String slot;
 
     /**
-     * @param moduleName the name of the module to be removed
-     * @param slot       specifies a slot which should be removed. If this argument is not specified, "main" slot
-     *                   is assumed
+     * @param moduleName the name of the module to be removed (assumes the {@code main} slot)
      */
-    public RemoveModule(String moduleName, String slot) {
-        this.moduleName = moduleName;
-        this.slot = slot;
+    public RemoveModule(String moduleName) {
+        this(moduleName, AddModule.DEFAULT_SLOT);
     }
 
     /**
      * @param moduleName the name of the module to be removed
+     * @param slot       specifies a slot which should be removed
      */
-    public RemoveModule(String moduleName) {
-        this(moduleName, AddModule.DEFAULT_SLOT);
+    public RemoveModule(String moduleName, String slot) {
+        if (moduleName == null) {
+            throw new IllegalArgumentException("moduleName cannot be null");
+        }
+        if (slot == null) {
+            throw new IllegalArgumentException("slot cannot be null");
+        }
+
+        this.moduleName = moduleName;
+        this.slot = slot;
     }
 
     @Override
@@ -37,10 +42,7 @@ public final class RemoveModule implements OnlineCommand {
             throw new CommandFailedException("RemoveModule command isn't supported in domain mode");
         }
 
-        StringBuilder cmd = new StringBuilder("module remove");
-        cmd.append(" --name=" + moduleName);
-        cmd.append(" --slot=" + slot);
-        ctx.client.executeCli(cmd.toString());
+        ctx.client.executeCli("module remove --name=" + moduleName + " --slot=" + slot);
     }
 
     @Override
